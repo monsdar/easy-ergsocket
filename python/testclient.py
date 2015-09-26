3a54cb452aaf23def1143fc6f09e23f514c6e625
Nils Brinkmann <monsdar+github@gmail.com>
2015-09-26 18:47:46 +0200

    Added: Python client which receives and unpacks the data

---
+++ python/testclient.py
@@ -0,0 +1,24 @@
+
+import Erg_pb2
+import sys
+import zmq
+
+#  Socket to talk to server
+context = zmq.Context()
+socket = context.socket(zmq.SUB)
+
+print("Connecting to Publisher")
+socket.connect("tcp://as1331:5556")
+
+#Subscribe to the right envelope
+# Ascii bytes to unicode str is needed here
+envelope = "EasyErgsocket"
+if isinstance(envelope, bytes):
+    envelope = envelope.decode('ascii')
+socket.setsockopt_string(zmq.SUBSCRIBE, envelope)
+
+while True:
+    currentMessage = socket.recv_multipart()
+    erg = Erg_pb2.Erg()
+    erg.ParseFromString(currentMessage[1])
+    print erg.displayedMeters
